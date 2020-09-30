e3156fbc85ba567df2b0cc5cb985f4c932b14e9c
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-09-30 11:46:08 -0300

    Created a class to aid in tests.

---
+++ Source/ProjectR/Tests/Utilities/PIESessionUtilities.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * This class is intended to use in tests only and when the editor is playing a PIE session
+ */
+class PROJECTR_API PIESessionUtilities
+{
+public:
+	PIESessionUtilities();
+	~PIESessionUtilities();
+};
