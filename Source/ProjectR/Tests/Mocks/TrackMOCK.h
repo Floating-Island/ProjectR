da974aeba02b83e73aa79e8b86c0a99082ced11b
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-10-05 12:19:40 -0300

    Created a Track mock to aid on tests.

---
+++ Source/ProjectR/Tests/Mocks/TrackMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Track.h"
+#include "TrackMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ATrackMOCK : public ATrack
+{
+	GENERATED_BODY()
+	
+};
