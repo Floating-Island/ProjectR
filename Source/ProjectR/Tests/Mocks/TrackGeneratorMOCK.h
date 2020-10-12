4dd66a0d4c293a4d2cefeab4f0d11f8085ca2847
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-10-12 11:19:30 -0300

    Added a track generator mock class.

---
+++ Source/ProjectR/Tests/Mocks/TrackGeneratorMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Track/TrackGenerator.h"
+#include "TrackGeneratorMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ATrackGeneratorMOCK : public ATrackGenerator
+{
+	GENERATED_BODY()
+	
+};
