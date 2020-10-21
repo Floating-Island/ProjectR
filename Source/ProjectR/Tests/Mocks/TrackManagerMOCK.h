6d23b4f0dec70fad7976b3c0986a89f65be57069
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-10-21 09:23:31 -0300

    Created a TrackManager mock to fulfill the test.

---
+++ Source/ProjectR/Tests/Mocks/TrackManagerMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Track/TrackManager.h"
+#include "TrackManagerMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ATrackManagerMOCK : public ATrackManager
+{
+	GENERATED_BODY()
+	
+};
