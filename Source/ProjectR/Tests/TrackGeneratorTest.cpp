9674e22600b2a7e6a55d7730c1484c1d55aa1fb4
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-04 12:52:14 -0300

    Added missing include.

--- Source/ProjectR/Tests/TrackGeneratorTest.cpp
+++ Source/ProjectR/Tests/TrackGeneratorTest.cpp
@@ -5,6 +5,7 @@
 
 #include "Track/TrackGenerator.h"
 #include "Mocks/TrackGeneratorMOCK.h"
+#include "Track/TrackManager.h"
 
 #include "Misc/AutomationTest.h"
 #include "Tests/AutomationEditorCommon.h"
