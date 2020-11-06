643e6e2e8ca78228128c05539b189cbc3a86b10d
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-06 17:35:56 -0300

    Modified text.

--- Source/ProjectR/Tests/InitialLapPhaseTest.cpp
+++ Source/ProjectR/Tests/InitialLapPhaseTest.cpp
@@ -22,11 +22,11 @@
 //Pay attention to the automation flags because they're needed to run the tests without UI errors.
 
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapIsntNullWhenInstantiatedTest, "ProjectR.Lap Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapIsntNullWhenInstantiatedTest, "ProjectR.InitialLapPhase Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 bool FALapIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
 {
-	AInitialLapPhase* testLap = NewObject<AInitialLapPhase>();
+	AInitialLapPhase* testInitialPhase = NewObject<AInitialLapPhase>();
 
 	TestNotNull(TEXT("The lap shouldn't be null after instantiating it."), testLap);
 
@@ -38,14 +38,14 @@ bool FALapIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
 
 
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapUpdateStateReturnsPhase1IfItsPassedAsParameterTest, "ProjectR.Lap Tests.Unit.001: If a APhase1 instance is passed to updateState, it returns the APhase1 instance", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapUpdateStateReturnsIntermediateLapPhaseIfItsPassedAsParameterTest, "ProjectR.InitialLapPhase Tests.Unit.001: If a AIntermediateLapPhase instance is passed to updateState, it returns that instance", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
-bool FALapUpdateStateReturnsPhase1IfItsPassedAsParameterTest::RunTest(const FString& Parameters)
+bool FALapUpdateStateReturnsIntermediateLapPhaseIfItsPassedAsParameterTest::RunTest(const FString& Parameters)
 {
-	AInitialLapPhase* testLap = NewObject<AInitialLapPhase>();
-	AIntermediateLapPhase* testPhase1 = NewObject<AIntermediateLapPhase>();
+	AInitialLapPhase* testInitialPhase = NewObject<AInitialLapPhase>();
+	AIntermediateLapPhase* testIntermediateLapPhase = NewObject<AIntermediateLapPhase>();
 
-	TestTrue(TEXT("The lap should return the Phase1 instance if that is passed as parameter."), testLap->updatePhase(testPhase1) == testPhase1);
+	TestTrue(TEXT("The initial phase should return the IntermediateLapPhase instance if that is passed as parameter."), testInitialPhase->updatePhase(testIntermediateLapPhase) == testIntermediateLapPhase);
 
 	return true;
 }
@@ -55,14 +55,14 @@ bool FALapUpdateStateReturnsPhase1IfItsPassedAsParameterTest::RunTest(const FStr
 
 
 
-IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapUpdateStateReturnsItselfIfPhase1IsntPassedAsParameterTest, "ProjectR.Lap Tests.Unit.001: Returns itself if a APhase1 instance as parameter in updateState", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapUpdateStateReturnsItselfIfIntermediateLapPhaseIsntPassedAsParameterTest, "ProjectR.InitialLapPhase Tests.Unit.002: Returns itself if a AIntermediateLapPhase instance as parameter in updateState", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
-bool FALapUpdateStateReturnsItselfIfPhase1IsntPassedAsParameterTest::RunTest(const FString& Parameters)
+bool FALapUpdateStateReturnsItselfIfIntermediateLapPhaseIsntPassedAsParameterTest::RunTest(const FString& Parameters)
 {
-	AInitialLapPhase* testLap = NewObject<AInitialLapPhase>();
-	AIntermediateLapPhase* testPhase1 = NewObject<AIntermediateLapPhase>();
+	AInitialLapPhase* testInitialPhase = NewObject<AInitialLapPhase>();
+	AIntermediateLapPhase* testIntermediateLapPhase = NewObject<AIntermediateLapPhase>();
 
-	TestTrue(TEXT("The lap should return itself if a Phase1 instance ins't passed as parameter of updateState."), testLap->updatePhase(NewObject<ALapPhase>()) == testPhase1);
+	TestTrue(TEXT("The initial phase should return itself if a IntermediateLapPhase instance ins't passed as parameter of updateState."), testInitialPhase->updatePhase(NewObject<ALapPhase>()) == testIntermediateLapPhase);
 
 	return true;
 }
