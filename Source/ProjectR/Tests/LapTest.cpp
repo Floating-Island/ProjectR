fd3fa193b438da8b81682aa5bd581c20f3a3cf24
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-06 16:49:41 -0300

    Created a test to check if the lap isn't null when instantiated.

--- Source/ProjectR/Tests/LapTest.cpp
+++ Source/ProjectR/Tests/LapTest.cpp
@@ -3,3 +3,33 @@
 
 #include "LapTest.h"
 
+#include "LapPhases/Lap.h"
+#include "Misc/AutomationTest.h"
+
+
+
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+//each one of this tests should test something of the project class that this test class references to.
+//Each project class should have a test class for it. It's something kind of necessary for TDD.
+
+//It's nice if the prettyname follows a pattern like: Game.ClassToTest.Unit.TestName
+//TestName should express what you expect from a test given a scenario.
+//Pay attention to the automation flags because they're needed to run the tests without UI errors.
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapIsntNullWhenInstantiatedTest, "ProjectR.Lap Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FALapIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	ALap* testLap = NewObject<ALap>();
+
+	TestNotNull(TEXT("The lap shouldn't be null after instantiating it."), testLap);
+
+	return true;
+}
+
+#endif //WITH_DEV_AUTOMATION_TESTS
