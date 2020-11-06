43009201be3de9381ef0e4c7a7c995857c6d3acc
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-06 15:57:01 -0300

    Created a test to check that the lap phase has collision enabled to query only.

--- Source/ProjectR/Tests/LapPhaseTest.cpp
+++ Source/ProjectR/Tests/LapPhaseTest.cpp
@@ -81,5 +81,66 @@ bool FALapPhaseHasPhaseWallAsRootTest::RunTest(const FString& Parameters)
 
 
 
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnALapPhaseMOCKInEditorWorldCommand);
+
+bool FSpawnALapPhaseMOCKInEditorWorldCommand::Update()
+{
+	if (GEditor->GetEditorWorldContext().World()->GetMapName() != "VoidWorld")
+	{
+		return false;
+	}
+
+	UWorld* testWorld = GEditor->GetEditorWorldContext().World();
+
+	testWorld->SpawnActor<ALapPhaseMOCK>(ALapPhaseMOCK::StaticClass());
+
+	return true;
+}
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckLapPhaseCollisionEnabledCommand, FAutomationTestBase*, test);
+
+bool FCheckLapPhaseCollisionEnabledCommand::Update()
+{
+	if (GEditor->GetEditorWorldContext().World()->GetMapName() != "VoidWorld")
+	{
+		return false;
+	}
+	UWorld* testWorld = GEditor->GetEditorWorldContext().World();
+	ALapPhaseMOCK* testPhase = Cast<ALapPhaseMOCK, AActor>(UGameplayStatics::GetActorOfClass(testWorld, ALapPhaseMOCK::StaticClass()));
+	if (testPhase)
+	{
+
+		bool collisionIsQueryOnly = testPhase->hasCollisionEnabledToQueryOnly();
+		UE_LOG(LogTemp, Log, TEXT("The phase wall has collision enabled to query only: %s."), *FString(collisionIsQueryOnly ? "true" : "false"));
+
+
+		test->TestTrue(TEXT("At spawning, the phase wall should have collision enabled to query only."), collisionIsQueryOnly);
+		return true;
+	}
+	return false;
+}
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseHasCollisionEnabledToQueryOnlyTest, "ProjectR.LapPhase Tests.Unit.004: LapPhase has collision enabled to query only at spawning", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FALapPhaseHasCollisionEnabledToQueryOnlyTest::RunTest(const FString& Parameters)
+{
+
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FSpawnALapPhaseMOCKInEditorWorldCommand);
+
+	ADD_LATENT_AUTOMATION_COMMAND(FCheckLapPhaseCollisionEnabledCommand(this));
+
+	return true;
+}
+
+
+
+
+
 
 #endif //WITH_DEV_AUTOMATION_TESTS
