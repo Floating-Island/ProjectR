28afaa0e3adfed95a85ed8b0002298210c595d94
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-06 16:46:08 -0300

    Added each test class for the different lap phases.

---
+++ Source/ProjectR/Tests/Phase1Test.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Phase1Test.h"
+
+Phase1Test::Phase1Test()
+{
+}
+
+Phase1Test::~Phase1Test()
+{
+}
---
+++ Source/ProjectR/Tests/Phase2Test.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Phase2Test.h"
+
+Phase2Test::Phase2Test()
+{
+}
+
+Phase2Test::~Phase2Test()
+{
+}
