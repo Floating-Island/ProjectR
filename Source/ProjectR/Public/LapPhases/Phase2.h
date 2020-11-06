a53d7ba8685e32e7905c7f59a6f4cc36386f9642
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-06 16:42:48 -0300

    Created the different lap phases. Phase1->Phase2->Lap.

---
+++ Source/ProjectR/Public/LapPhases/Phase2.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LapPhases/LapPhase.h"
+#include "Phase2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API APhase2 : public ALapPhase
+{
+	GENERATED_BODY()
+	
+};
