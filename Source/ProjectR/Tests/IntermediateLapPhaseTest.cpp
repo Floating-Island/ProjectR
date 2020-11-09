4b287cb295fa19d1a64395da5fb86c46a7ddac22
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-09 09:23:29 -0300

    Created test for intermediate lap phases to check instance isn't null when created.

--- Source/ProjectR/Tests/IntermediateLapPhaseTest.cpp
+++ Source/ProjectR/Tests/IntermediateLapPhaseTest.cpp
@@ -3,3 +3,34 @@
 
 #include "IntermediateLapPhaseTest.h"
 
+#include "LapPhases/IntermediateLapPhase.h"
+#include "Misc/AutomationTest.h"
+
+
+
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+//each one of this tests should test something of the project class that this test class references to.
+//Each project class should have a test class for it. It's something kind of necessary for TDD.
+
+//It's nice if the prettyname follows a pattern like: Game.ClassToTest.Unit.TestName
+//TestName should express what you expect from a test given a scenario.
+//Pay attention to the automation flags because they're needed to run the tests without UI errors.
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FAIntermediateLapPhaseIsntNullWhenInstantiatedTest, "ProjectR.IntermediateLapPhase Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FAIntermediateLapPhaseIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	AIntermediateLapPhase* testIntermediatePhase = NewObject<AIntermediateLapPhase>();
+
+	TestNotNull(TEXT("The intermediate lap phase shouldn't be null after instantiating it."), testIntermediatePhase);
+
+	return true;
+}
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
