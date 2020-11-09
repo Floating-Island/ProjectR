659b04adbef53db96e31dfcaa2ef5b4964d9f654
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-09 09:39:18 -0300

    Created a test to check that the final lap phase isn't null when instantiated.

--- Source/ProjectR/Tests/FinalLapPhaseTest.cpp
+++ Source/ProjectR/Tests/FinalLapPhaseTest.cpp
@@ -3,3 +3,34 @@
 
 #include "FinalLapPhaseTest.h"
 
+#include "LapPhases/FinalLapPhase.h"
+#include "Misc/AutomationTest.h"
+
+
+
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+//each one of this tests should test something of the project class that this test class references to.
+//Each project class should have a test class for it. It's something kind of necessary for TDD.
+
+//It's nice if the prettyname follows a pattern like: Game.ClassToTest.Unit.TestName
+//TestName should express what you expect from a test given a scenario.
+//Pay attention to the automation flags because they're needed to run the tests without UI errors.
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FAFinalLapPhaseIsntNullWhenInstantiatedTest, "ProjectR.FinalLapPhase Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FAFinalLapPhaseIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	AFinalLapPhase* testFinalPhase = NewObject<AFinalLapPhase>();
+
+	TestNotNull(TEXT("The final lap phase shouldn't be null after instantiating it."), testFinalPhase);
+
+	return true;
+}
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
