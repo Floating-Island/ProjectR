dc2092cfc8e526391c883144b03dfdc6b7c4a90d
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-09 15:08:28 -0300

    Made the lap manager store the jets spawned in the world.

--- Source/ProjectR/Private/LapManager/LapManager.cpp
+++ Source/ProjectR/Private/LapManager/LapManager.cpp
@@ -2,20 +2,36 @@
 
 
 #include "LapManager/LapManager.h"
+#include "Jet/Jet.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ALapManager::ALapManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	jetLaps = TSet<AJet*>();
 }
 
 // Called when the game starts or when spawned
 void ALapManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	storeJetsFromWorld();
+}
+
+void ALapManager::storeJetsFromWorld()
+{
+	TArray<AActor*> worldJets = TArray<AActor*>();
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(),AJet::StaticClass(),worldJets);
+	for (const auto& jet: worldJets)
+	{
+		AJet* castedJet = Cast<AJet,AActor>(jet);
+		if(castedJet)
+		{
+			jetLaps.Add(castedJet);
+		}
+	}
 }
 
 // Called every frame
@@ -23,5 +39,10 @@ void ALapManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+TSet<AJet*> ALapManager::jetsInPlay()
+{
+	return jetLaps;
 }
 
--- Source/ProjectR/Public/LapManager/LapManager.h
+++ Source/ProjectR/Public/LapManager/LapManager.h
@@ -6,14 +6,21 @@
 #include "GameFramework/Actor.h"
 #include "LapManager.generated.h"
 
+
+class AJet;
+
 UCLASS()
 class PROJECTR_API ALapManager : public AActor
 {
 	GENERATED_BODY()
-	
+
+private:
+	TSet<AJet*> jetLaps;
+	void storeJetsFromWorld();
 public:	
 	// Sets default values for this actor's properties
 	ALapManager();
+	
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,5 +29,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	TSet<AJet*> jetsInPlay();
 
 };
