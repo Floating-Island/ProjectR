bc6baa397de19f6d4efaf0ca54aea17e54316661
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-09 11:05:03 -0300

    Added a lap phase factory class to project.

---
+++ Source/ProjectR/Public/LapPhases/LapPhaseFactory.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "LapPhaseFactory.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ULapPhaseFactory : public UObject
+{
+	GENERATED_BODY()
+	
+};
