ca887ba92d493654f31e3cdac0a972ffcb20d893
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-11 10:07:46 -0300

    Created the race game mode.

---
+++ Source/ProjectR/Public/GameMode/RaceGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "RaceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ARaceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
