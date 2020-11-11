cc7dd75b99ad5ad8a74a68f8becb2bfd27e47f87
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-11 10:10:49 -0300

    Created a test to check that the race game mode isn't null when instanced.

--- Source/ProjectR/Tests/RaceGameModeTest.cpp
+++ Source/ProjectR/Tests/RaceGameModeTest.cpp
@@ -18,7 +18,16 @@
 //TestName should express what you expect from a test given a scenario.
 //Pay attention to the automation flags because they're needed to run the tests without UI errors.
 
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FARaceGameModeIsntNullWhenInstantiatedTest, "ProjectR.RaceGameMode Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
+bool FARaceGameModeIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	ARaceGameMode* testRaceGameMode = NewObject<ARaceGameMode>();
+
+	TestNotNull(TEXT("The race game mode shouldn't be null after instantiating it."), testRaceGameMode);
+
+	return true;
+}
 
 
 
