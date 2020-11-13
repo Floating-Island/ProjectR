f32ac8cd6c2ec29234e85cb45129cb3e28ab406b
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-13 16:17:18 -0300

    Created race stages classes.

---
+++ Source/ProjectR/Private/GameMode/RaceStages/RaceRunningStage.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/RaceStages/RaceRunningStage.h"
+
---
+++ Source/ProjectR/Public/GameMode/RaceStages/RacePreparationStage.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/RaceStages/RaceStage.h"
+#include "RacePreparationStage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ARacePreparationStage : public ARaceStage
+{
+	GENERATED_BODY()
+	
+};
