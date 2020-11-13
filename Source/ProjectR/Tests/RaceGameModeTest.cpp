4ac885a634932b917a4780255570395936007080
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-13 09:56:34 -0300

    Created a test to check that jets are positioned behind the initial lap pahse.

--- Source/ProjectR/Tests/RaceGameModeTest.cpp
+++ Source/ProjectR/Tests/RaceGameModeTest.cpp
@@ -6,6 +6,9 @@
 
 #include "GameMode/RaceGameMode.h"
 #include "Jet/Jet.h"
+#include "Track/TrackGenerator.h"
+#include "LapPhases/InitialLapPhase.h"
+#include "LapManager/LapManager.h"
 
 #include "Misc/AutomationTest.h"
 #include "Tests/AutomationEditorCommon.h"
@@ -62,7 +65,6 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FARaceGameModeIsAbleToBeSetInAWorldTest, "Proje
 
 bool FARaceGameModeIsAbleToBeSetInAWorldTest::RunTest(const FString& Parameters)
 {
-
 	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld-RaceGameMode");
 
 	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
@@ -126,7 +128,6 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FARaceGameModeCreatesExpectedNumberOfJetsTest, 
 
 bool FARaceGameModeCreatesExpectedNumberOfJetsTest::RunTest(const FString& Parameters)
 {
-
 	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld-RaceGameMode");
 
 	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
@@ -135,6 +136,85 @@ bool FARaceGameModeCreatesExpectedNumberOfJetsTest::RunTest(const FString& Param
 	int tickCount = 0;
 	int tickLimit = 2;
 	ADD_LATENT_AUTOMATION_COMMAND(FCheckRaceGameModeCreatesAllJetsCommand(tickCount, tickLimit, this));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
+	return true;
+}
+
+
+
+
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_THREE_PARAMETER(FCheckRaceGameModeJetsPositioningCommand, int, aTickCount, int, aTickLimit, FAutomationTestBase*, test);
+
+bool FCheckRaceGameModeJetsPositioningCommand::Update()
+{
+	if (!GEditor->IsPlayingSessionInEditor())
+	{
+		return false;
+	}
+	PIESessionUtilities sessionUtilities = PIESessionUtilities();
+	UWorld* testWorld = sessionUtilities.currentPIEWorld();
+
+	ARaceGameMode* testGameMode = Cast<ARaceGameMode, AGameModeBase>(UGameplayStatics::GetGameMode(testWorld));
+	if (testGameMode)
+	{
+		TSet<AJet*> gameModeJets = testGameMode->jetsInPlay();
+		int expectedNumberOfJets = testGameMode->jetsToSpawn();
+		bool sameNumberOfJetsInGameMode = gameModeJets.Num() == expectedNumberOfJets;
+		ATrackGenerator* testTrack = sessionUtilities.retrieveFromPIEAnInstanceOf<ATrackGenerator>();
+		AInitialLapPhase* initialPhase = testGameMode->initialLapPhase();
+		float distanceBetweenPhaseAndTrack = testTrack->distanceAlongSplineOf(initialPhase);
+
+		bool jetsAreBehindInitialPhase = true;
+		for (const auto& jet : gameModeJets)
+		{
+			float distanceBetweenJetAndTrack = testTrack->distanceAlongSplineOf(jet);
+			UE_LOG(LogTemp, Log, TEXT("Distance between jet and track: %f."), distanceBetweenJetAndTrack);
+			if (distanceBetweenJetAndTrack > distanceBetweenPhaseAndTrack)
+			{
+				jetsAreBehindInitialPhase = false;
+				break;
+			}
+		}
+
+		UE_LOG(LogTemp, Log, TEXT("Expected number of jets: %d."), expectedNumberOfJets);
+		UE_LOG(LogTemp, Log, TEXT("Number of race game mode jets: %d."), gameModeJets.Num());
+		UE_LOG(LogTemp, Log, TEXT("Distance between initial lap phase and track: %f."), distanceBetweenPhaseAndTrack);
+
+
+		if (sameNumberOfJetsInGameMode && jetsAreBehindInitialPhase)
+		{
+			test->TestTrue(TEXT("Race game mode should position the jets behind the initial lap phase."), sameNumberOfJetsInGameMode && jetsAreBehindInitialPhase);
+			testWorld->bDebugFrameStepExecution = true;
+			return true;
+		}
+
+		++aTickCount;
+		if (aTickCount > aTickLimit)
+		{
+			test->TestTrue(TEXT("Tick limit reached. Race game mode should position the jets behind the initial lap phase."), sameNumberOfJetsInGameMode && jetsAreBehindInitialPhase);
+			testWorld->bDebugFrameStepExecution = true;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FARaceGameModePositionsJetsBehindTheInitialLapPhaseTest, "ProjectR.RaceGameMode Tests.Integration.003: Positions jets behind the initial lap phase", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FARaceGameModePositionsJetsBehindTheInitialLapPhaseTest::RunTest(const FString& Parameters)
+{
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld-RaceGameMode");
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
+
+	int tickCount = 0;
+	int tickLimit = 2;
+	ADD_LATENT_AUTOMATION_COMMAND(FCheckRaceGameModeJetsPositioningCommand(tickCount, tickLimit, this));
 
 	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
 	return true;
