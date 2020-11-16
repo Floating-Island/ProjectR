0614199901eec28bc8e7f6052595d550931ebed7
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-16 16:27:27 -0300

    Created a race stage mock.

---
+++ Source/ProjectR/Tests/Mocks/RaceStageMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/RaceStages/RaceStage.h"
+#include "RaceStageMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ARaceStageMOCK : public ARaceStage
+{
+	GENERATED_BODY()
+	
+};
