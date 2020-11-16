1143c65c52841f52b6b5cc6afd8e2e92a8f2e845
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-16 15:08:46 -0300

    Created a test to check that the race preparation stage isn't null when instantiated.

--- Source/ProjectR/Tests/RacePreparationStageTest.cpp
+++ Source/ProjectR/Tests/RacePreparationStageTest.cpp
@@ -18,7 +18,16 @@
 //TestName should express what you expect from a test given a scenario.
 //Pay attention to the automation flags because they're needed to run the tests without UI errors.
 
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FARacePreparationStageIsntNullWhenInstantiatedTest, "ProjectR.RacePreparationStage Tests.Unit.000: Isn't null when instantiated", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
+bool FARacePreparationStageIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	ARacePreparationStage* testRacePreparationStage = NewObject<ARacePreparationStage>();
+
+	TestNotNull(TEXT("The race preparation stage shouldn't be null after instantiating it."), testRacePreparationStage);
+
+	return true;
+}
 
 
 #endif //WITH_DEV_AUTOMATION_TESTS
