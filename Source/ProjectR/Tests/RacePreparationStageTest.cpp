6bf0b36696724f87573590da5321ae3634ef5193
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-16 15:25:21 -0300

    Created a test to check that the race preparation stage nextStage spawns and returns a  race beginning stage.

--- Source/ProjectR/Tests/RacePreparationStageTest.cpp
+++ Source/ProjectR/Tests/RacePreparationStageTest.cpp
@@ -3,9 +3,14 @@
 
 #include "RacePreparationStageTest.h"
 
+
+
 #include "GameMode/RaceStages/RacePreparationStage.h"
+#include "GameMode/RaceStages/RaceBeginningStage.h"
 
 #include "Misc/AutomationTest.h"
+#include "Tests/AutomationEditorCommon.h"
+#include "Utilities/PIESessionUtilities.h"
 
 
 #if WITH_DEV_AUTOMATION_TESTS
@@ -28,6 +33,48 @@ bool FARacePreparationStageIsntNullWhenInstantiatedTest::RunTest(const FString& 
 
 	return true;
 }
+
+
+
+
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FSpawnARacePreparationStageCommand, FAutomationTestBase*, test);
+
+bool FSpawnARacePreparationStageCommand::Update()
+{
+	if (!GEditor->IsPlayingSessionInEditor())
+	{
+		return false;
+	}
+	PIESessionUtilities sessionUtilities = PIESessionUtilities();
+	UWorld* testWorld = sessionUtilities.currentPIEWorld();
+	
+	ARacePreparationStage* testPreparation = sessionUtilities.spawnInPIEAnInstanceOf<ARacePreparationStage>();
+	ARaceBeginningStage* testBeginning = Cast<ARaceBeginningStage,ARaceStage>(testPreparation->nextStage());
+
+	test->TestNotNull(TEXT("The race preparation stage's nextStage should spawn a race beginning stage in the world."), testBeginning);
+	testWorld->bDebugFrameStepExecution = true;
+	return true;
+
+}
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FARaceGameModeCreatesExpectedNumberOfJetsTest, "ProjectR.RacePreparationStage Tests.Unit.002: nextStage spawns a race beginning stage", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+bool FARaceGameModeCreatesExpectedNumberOfJetsTest::RunTest(const FString& Parameters)
+{
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld-RaceGameMode");
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FSpawnARacePreparationStageCommand(this));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
+	return true;
+}
+
 
 
 #endif //WITH_DEV_AUTOMATION_TESTS
