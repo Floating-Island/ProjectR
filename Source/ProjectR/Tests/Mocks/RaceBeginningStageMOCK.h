d10c1dd0899360751606b758e762fab12e3ed16c
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-17 12:35:42 -0300

    Created a race beginning mock.

---
+++ Source/ProjectR/Tests/Mocks/RaceBeginningStageMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameMode/RaceStages/RaceBeginningStage.h"
+#include "RaceBeginningStageMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ARaceBeginningStageMOCK : public ARaceBeginningStage
+{
+	GENERATED_BODY()
+	
+};
