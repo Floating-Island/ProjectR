061c7a85c2230a37ecf22974c5c75c7f32dad1e5
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-24 13:47:38 -0300

    Separated declaration from definition.

--- Source/ProjectR/Tests/Commands/TrackGeneratorTestCommands.h
+++ Source/ProjectR/Tests/Commands/TrackGeneratorTestCommands.h
@@ -2,14 +2,130 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
-
 /**
- * 
+ * This file cointains the latent commands necessary for the tests.
  */
-class PROJECTR_API TrackGeneratorTest
-{
-public:
-	TrackGeneratorTest();
-	~TrackGeneratorTest();
-};
+
+#include "Misc/AutomationTest.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+//Test preparation commands:
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnTrackGeneratorInEditorWorldCommand);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FSpawnTrackGeneratorInEditorWorldRollSplineComponentsCommand, float, rollValue);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FSpawnTrackGeneratorInEditorWorldWidenSplineComponentsCommand, float, widthValue);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnTrackGeneratorInEditorWorldDisableCollisionsCommand);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnTrackGeneratorInPIECommand);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnTwoTrackGeneratorsInPIECommand);
+
+
+
+
+
+//Test check commands:
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesQuantityCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesStartPositionsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesEndPositionsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesStartTangentsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesEndTangentsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesMeshesCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesMeshesAreRoadMeshCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckSplineMagnetSplinesQuantityCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesCollisionEnabledCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesCollisionObjectTypeCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesAttachToRootCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRoadSplinesMobilityCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesMobilityCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesAttachToRoadSplinesCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesOnTopRoadSplinesCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesTangentsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesMeshesCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesVisibilityCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesCollisionResponseCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesResponseToPawnChannelCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckMagnetSplinesGenerateOverlapEventsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckComponentsSmoothInterpolationCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckTrackSectionsArrayQuantityAdjustedToNumberOfSplinePointsCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_TWO_PARAMETER(FCheckComponentsRollCommand, float, rollValue, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_TWO_PARAMETER(FCheckComponentsWidthCommand, float, widthValue, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckComponentsCollisionCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckComponentsExpectedCollisionInPIECommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckTrackGeneratorSpawnsTrackManagerInPIECommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckTrackGeneratorsSpawnOneTrackManagerInPIECommand, FAutomationTestBase*, test);
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
--- Source/ProjectR/Tests/TrackGeneratorTest.h
+++ Source/ProjectR/Tests/TrackGeneratorTest.h
@@ -2,14 +2,238 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 
 /**
+ * Each one of this tests should test something of the project class that this test class references to.
+ * Each project class should have a test class for it. It's something kind of necessary for TDD.
  * 
+ * It's nice if the prettyname follows a pattern like: Game.ClassToTest.Unit.TestName
+ * TestName should express what you expect from a test given a scenario.
+ * Pay attention to the automation flags because they're needed to run the tests without UI errors.
  */
-class PROJECTR_API TrackGeneratorTest
-{
-public:
-	TrackGeneratorTest();
-	~TrackGeneratorTest();
-};
+
+#include "Misc/AutomationTest.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorIsntNullWhenInstantiatedTest, 
+	"ProjectR.TrackGenerator Tests.Unit.000: Isn't null when instantiated", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorHasASplineComponentTest, 
+	"ProjectR.TrackGenerator Tests.Unit.001: Has a spline component", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorSplineComponentIsTheRootComponentTest, 
+	"ProjectR.TrackGenerator Tests.Unit.002: Spline component is the root component", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesQuantityIsTheSameAsSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.003: Road splines quantity is the same as spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesStartPositionsAreTheSameAsSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.004: Road splines start positions are the same as spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesEndPositionsAreTheSameAsNextSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.005: Road splines end positions are the same as next spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesStartTangentsAreTheSameAsSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.006: Road splines start tangents are the same as spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesEndTangentsAreTheSameAsNextSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.007: Road splines end tangents are the same as next spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesMeshesAreSetAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.008: Road splines meshes are set at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorRoadSplinesMeshesAreTheRoadMeshAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.009: Road splines meshes are the road mesh at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorHasTheSameAmountOfMagnetSplinesAsSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.010: Has the same amount of magnet splines as spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMeshesHaveCollisionEnabledAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.011: Meshes have collision enabled at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMeshesHaveCollisionObjectTypeWorldStaticAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.012: Meshes have collision object type world static at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMeshesAreAttachedToRootAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.013: Meshes are attached to root at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMeshesHaveSameMobilityAsRootAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.014: Meshes have same mobility as root at spawning",
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorSplineComponentLoopsTest, 
+	"ProjectR.TrackGenerator Tests.Unit.015: Spline component loops", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesHaveTheSameMobilityAsRoadSplinesAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.016: Magnet splines have the same mobility as road splines at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesAreAttachedToRoadSplinesAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.017: Magnet splines are attached to road splines at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesAreOnTopOfRoadSplinesAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.018: Magnet splines are on top of road splines at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesTangentsAreTheSameAsSplinePointsAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.019: Magnet splines tangents are the same as spline points at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesMeshesAreSetAtSpawningTest, 
+	"ProjectR.TrackGenerator Tests.Unit.020: Magnet splines meshes are set at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesAreHiddenInGameTest, 
+	"ProjectR.TrackGenerator Tests.Unit.021: Magnet splines are hidden in game", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesHaveCollisionEnabledToQueryOnlyTest, 
+	"ProjectR.TrackGenerator Tests.Unit.022: Magnet splines have collision enabled to query only", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesOverlapWithPawnChannelTest, 
+	"ProjectR.TrackGenerator Tests.Unit.023: Magnet splines overlap with pawn channel", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorMagnetSplinesGenerateOverlapEventsTest, 
+	"ProjectR.TrackGenerator Tests.Unit.024: Magnet splines generate overlap events", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorSplineMeshComponentsHaveSmoothInterpolationEnabledTest, 
+	"ProjectR.TrackGenerator Tests.Unit.025: Spline mesh components have smooth interpolation enabled", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorGeneratesOverlapEventsWhenSpawnedTest, 
+	"ProjectR.TrackGenerator Tests.Unit.026: Generates overlap events when spawned", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorTrackSectionsQuantityMatchesSplinePointsNumberTest, 
+	"ProjectR.TrackGenerator Tests.Unit.027: Track sections quantity is adjusted to the number of spline points", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorSplineMeshComponentsRollAfterSettingRollInEditorTest, 
+	"ProjectR.TrackGenerator Tests.Unit.028: Spline mesh components modify their roll when setting it", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorSplineMeshComponentsWidenAfterSettingWidenInEditorTest, 
+	"ProjectR.TrackGenerator Tests.Unit.029: Spline mesh components modify their width when setting it", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorAllowsDisableCollisionsInEditorTest, 
+	"ProjectR.TrackGenerator Tests.Unit.030: Spline mesh components disable collisions when setting it disabled", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorHaveExpectedCollisionsInPIETest, 
+	"ProjectR.TrackGenerator Tests.Unit.031: Spline mesh components in PIE have their expected collisions", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorSpawnTrackManagerAtBeginPlayTest, 
+	"ProjectR.TrackGenerator Tests.Unit.032: Track generator spawns a track manager at begin play", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FATrackGeneratorsSpawnOnlyOneTrackManagerAtBeginPlayTest, 
+	"ProjectR.TrackGenerator Tests.Unit.033: Track generators spawn only one track manager at begin play", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+
+//make the track generator create a track manager after recreating spline meshes on begin play. This way, the track manager is always created after the track generator. Update tests from track manager.
+//But only one track manager should be in the game so the next test is that if there are two track generators, only the first spawns a track manager:
+//1)retrieve all track managers.
+//2)If there's none, create one. Tell it to subscribe to this track generator.
+//3)if there's, tell it to subscribe to this track generator.
+//4)If there's more than one, throw an exception.
+//(when a custom mesh for magnet spline is already made) set location of magnet spline same as spline mesh,
+// attach and elevate the same amount as the bound of mesh (saved in constructor) multiplied by the scale (gotten in on construction).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
