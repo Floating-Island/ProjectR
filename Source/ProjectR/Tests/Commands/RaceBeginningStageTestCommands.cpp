4827fd13989869e2cfc2ea463597f9207667e765
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-24 11:51:09 -0300

    Separated declaration from definition.

--- Source/ProjectR/Tests/Commands/RaceBeginningStageTestCommands.cpp
+++ Source/ProjectR/Tests/Commands/RaceBeginningStageTestCommands.cpp
@@ -8,12 +8,93 @@
 #if WITH_DEV_AUTOMATION_TESTS
 
 #include "RaceBeginningStageTestCommands.h"
+#include "GameMode/RaceStages/RaceBeginningStage.h"
+#include "../Mocks/RaceBeginningStageMOCK.h"
+#include "GameMode/RaceStages/RaceRunningStage.h"
+
+
+#include "Tests/AutomationEditorCommon.h"
+#include "../Utilities/PIESessionUtilities.h"
 
 //Test preparation commands:
+
+bool FSpawnARaceBeginningMOCKCommand::Update()
+{
+	if (!GEditor->IsPlayingSessionInEditor())
+	{
+		return false;
+	}
+	PIESessionUtilities sessionUtilities = PIESessionUtilities();
+	ARaceBeginningStageMOCK* testBeginning = sessionUtilities.spawnInPIEAnInstanceOf<ARaceBeginningStageMOCK>();
+
+	testBeginning->start();
+	return true;
+}
+
+
+bool FSpawnARaceBeginningCallCountdownStartCommand::Update()
+{
+	if (!GEditor->IsPlayingSessionInEditor())
+	{
+		return false;
+	}
+	PIESessionUtilities sessionUtilities = PIESessionUtilities();
+	ARaceBeginningStage* testBeginning = sessionUtilities.spawnInPIEAnInstanceOf<ARaceBeginningStage>();
+
+	testBeginning->nextStage();
+	return true;
+}
+
+
+
+
+
+
 
 
 
 //Test check commands:
+
+bool FCheckTimerActiveCommand::Update()
+{
+	if (!GEditor->IsPlayingSessionInEditor())
+	{
+		return false;
+	}
+	PIESessionUtilities sessionUtilities = PIESessionUtilities();
+	UWorld* testWorld = sessionUtilities.currentPIEWorld();
+
+	ARaceBeginningStageMOCK* testBeginning = sessionUtilities.retrieveFromPIEAnInstanceOf<ARaceBeginningStageMOCK>();
+	if (testBeginning)
+	{
+		test->TestTrue(TEXT("Race beginning stage should have its timer active at start."), testBeginning->hasTimerActive());
+		testWorld->bDebugFrameStepExecution = true;
+		return true;
+	}
+	return false;
+}
+
+
+bool FCheckRunningStageSpawnedCommand::Update()
+{
+	if (!GEditor->IsPlayingSessionInEditor())
+	{
+		return false;
+	}
+	PIESessionUtilities sessionUtilities = PIESessionUtilities();
+	UWorld* testWorld = sessionUtilities.currentPIEWorld();
+
+	ARaceBeginningStage* testBeginning = sessionUtilities.retrieveFromPIEAnInstanceOf<ARaceBeginningStage>();
+	if (testBeginning)
+	{
+		ARaceRunningStage* testRunning = sessionUtilities.retrieveFromPIEAnInstanceOf<ARaceRunningStage>();
+		
+		test->TestNotNull(TEXT("Race beginning next stage should spawn a race running stage."), testRunning);
+		testWorld->bDebugFrameStepExecution = true;
+		return true;
+	}
+	return false;
+}
 
 
 
--- Source/ProjectR/Tests/Commands/RaceBeginningStageTestCommands.h
+++ Source/ProjectR/Tests/Commands/RaceBeginningStageTestCommands.h
@@ -6,14 +6,30 @@
  * This file cointains the latent commands necessary for the tests.
  */
 
+#include "Misc/AutomationTest.h"
+
 #if WITH_DEV_AUTOMATION_TESTS
 
 
 //Test preparation commands:
 
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnARaceBeginningMOCKCommand);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnARaceBeginningCallCountdownStartCommand);
+
+
+
 
 
 //Test check commands:
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckTimerActiveCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckRunningStageSpawnedCommand, FAutomationTestBase*, test);
+
+
 
 
 
