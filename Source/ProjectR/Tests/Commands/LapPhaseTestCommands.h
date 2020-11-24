981f6d75af4c3254fdf71c205204b39b6fcaa961
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-24 11:41:45 -0300

    Separated declaration from definition.

--- Source/ProjectR/Tests/Commands/LapPhaseTestCommands.h
+++ Source/ProjectR/Tests/Commands/LapPhaseTestCommands.h
@@ -8,13 +8,29 @@
 
 #if WITH_DEV_AUTOMATION_TESTS
 
+#include "Misc/AutomationTest.h"
 
 //Test preparation commands:
+
+DEFINE_LATENT_AUTOMATION_COMMAND(FSpawnALapPhaseMOCKInEditorWorldCommand);
+
+
+
 
 
 
 //Test check commands:
 
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckLapPhaseCollisionEnabledCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckLapPhaseCollisionResponseCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckLapPhaseObjectTypeCommand, FAutomationTestBase*, test);
+
+
+DEFINE_LATENT_AUTOMATION_COMMAND_ONE_PARAMETER(FCheckLapPhaseOverlapEventsCommand, FAutomationTestBase*, test);
 
 
 #endif //WITH_DEV_AUTOMATION_TESTS
--- Source/ProjectR/Tests/LapPhaseTest.h
+++ Source/ProjectR/Tests/LapPhaseTest.h
@@ -2,13 +2,79 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 
 /**
+ * Each one of this tests should test something of the project class that this test class references to.
+ * Each project class should have a test class for it. It's something kind of necessary for TDD.
  * 
+ * It's nice if the prettyname follows a pattern like: Game.ClassToTest.Unit.TestName
+ * TestName should express what you expect from a test given a scenario.
+ * Pay attention to the automation flags because they're needed to run the tests without UI errors.
  */
-class PROJECTR_API LapPhaseTest
-{
-public:
 
-};
+#include "Misc/AutomationTest.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseIsntNullWhenInstantiatedTest, 
+	"ProjectR.LapPhase Tests.Unit.000: Isn't null when instantiated", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseHasAStaticMeshComponentTest, 
+	"ProjectR.LapPhase Tests.Unit.001: Has a static mesh component", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseHasAStaticMeshTest, 
+	"ProjectR.LapPhase Tests.Unit.002: Has a static mesh associated", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseHasPhaseWallAsRootTest, 
+	"ProjectR.LapPhase Tests.Unit.003: The phase wall is the root component", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseHasCollisionEnabledToQueryOnlyTest, 
+	"ProjectR.LapPhase Tests.Unit.004: Has collision enabled to query only at spawning", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseOverlapsWithPawnChannelTest, 
+	"ProjectR.LapPhase Tests.Unit.005: Overlaps with the pawn channel", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseObjectTypeIsWorldStaticTest, 
+	"ProjectR.LapPhase Tests.Unit.006: Its object type is world static", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseGeneratesOverlapEventsTest, 
+	"ProjectR.LapPhase Tests.Unit.007: Generates overlap events", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseUpdateStateReturnsItselfTest, 
+	"ProjectR.LapPhase Tests.Unit.008: updateState returns itself", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FALapPhaseIsHiddenInGameTest, 
+	"ProjectR.LapPhase Tests.Unit.009: Is hidden in game", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
