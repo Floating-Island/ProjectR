54902572692701fdf0fb90587253a19dde49fc8b
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-24 10:01:22 -0300

    Created files to reorganize tests code. Now tests declarations, implementations, latent commands declarations and implementations are all separated.

---
+++ Source/ProjectR/Tests/Commands/JetMOCKTestCommands.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+
+
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+#include "JetMOCKTestCommands.h"
+
+//Test preparation commands:
+
+
+
+//Test check commands:
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
---
+++ Source/ProjectR/Tests/Commands/PlaceholderTestCommands.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * This file cointains the latent commands necessary for the tests.
+ */
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+//Test preparation commands:
+
+
+
+//Test check commands:
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
