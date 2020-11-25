7382edb6cfa37812fba76318ad44cfc0bb1d165e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-25 12:33:32 -0300

    Created a main menu mock.

---
+++ Source/ProjectR/Tests/Mocks/MainMenuMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/MainMenu.h"
+#include "MainMenuMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UMainMenuMOCK : public UMainMenu
+{
+	GENERATED_BODY()
+	
+};
