5b07e3071feaf2f7c1144cd8364442b0e47c165e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-26 15:41:20 -0300

    Created the project's game instance test class.

---
+++ Source/ProjectR/Tests/ProjectRGameInstanceTest.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "ProjectRGameInstanceTest.h"
+#include "Commands/ProjectRGameInstanceTestCommands.h"
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
