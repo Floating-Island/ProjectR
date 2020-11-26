58a5e393baf4896691596aaa8201742a189bc951
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-26 12:47:22 -0300

    Main menu, as it's a UI element, needs a blueprint to bind its variables, so it can't be instanced without it. I'll create a game instance to insert it and then call the game instance from the tests.

---
+++ Source/ProjectR/Public/GameInstance/ProjectRGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ProjectRGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UProjectRGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
