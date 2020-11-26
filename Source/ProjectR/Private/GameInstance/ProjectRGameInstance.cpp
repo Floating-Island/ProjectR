d17b16a43c7f07b963b96956af19d775af1d3839
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-26 16:13:20 -0300

    Made the game instance load the game menu when calling loadGameMenu.

--- Source/ProjectR/Private/GameInstance/ProjectRGameInstance.cpp
+++ Source/ProjectR/Private/GameInstance/ProjectRGameInstance.cpp
@@ -2,4 +2,15 @@
 
 
 #include "GameInstance/ProjectRGameInstance.h"
+#include "UI/MainMenu.h"
+#include "Blueprint/UserWidget.h"
 
+UMainMenu* UProjectRGameInstance::loadMainMenu()
+{
+	if(!mainMenu)
+	{
+		mainMenu = CreateWidget<UMainMenu>(this, mainMenuClass, FName("Main Menu"));
+	}
+	mainMenu->AddToViewport();
+	return mainMenu;
+}
--- Source/ProjectR/Public/GameInstance/ProjectRGameInstance.h
+++ Source/ProjectR/Public/GameInstance/ProjectRGameInstance.h
@@ -6,6 +6,7 @@
 #include "Engine/GameInstance.h"
 #include "ProjectRGameInstance.generated.h"
 
+class UMainMenu;
 /**
  * 
  */
@@ -13,5 +14,13 @@ UCLASS()
 class PROJECTR_API UProjectRGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UMainMenu* mainMenu;
+public:
+	UPROPERTY(EditDefaultsOnly, Category= "Menus")
+		TSubclassOf<UMainMenu> mainMenuClass;
+
+	UFUNCTION(BlueprintCallable)
+	UMainMenu* loadMainMenu();
 };
