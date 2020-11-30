acff9b74f64b53a87435f7d4eedf80d120313495
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-30 15:50:13 -0300

    Created the solo race menu.

---
+++ Source/ProjectR/Public/UI/SoloRaceMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SoloRaceMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API USoloRaceMenu : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
