f0a72327ea896ff41e4259488dbf53159f40b98f
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-11-30 15:50:30 -0300

    Created the files to test the solo race menu.

---
+++ Source/ProjectR/Tests/Commands/SoloRaceMenuTestCommands.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+#include "SoloRaceMenuTestCommands.h"
+#include "UI/SoloRaceMenu.h"
+
+//Test preparation commands:
+
+
+
+
+
+//Test check commands:
+
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
---
+++ Source/ProjectR/Tests/SoloRaceMenuTest.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+#include "SoloRaceMenuTest.h"
+#include "Commands/SoloRaceMenuTestCommands.h"
+#include "Tests/AutomationEditorCommon.h"
+
+#include "UI/SoloRaceMenu.h"
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
