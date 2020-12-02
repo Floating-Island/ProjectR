b9df9d359489e415433858ff4c01ac217bc40c67
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-02 12:20:10 -0300

    Created a base class for menus. Initially it'll hold the method for calculating a button's center position.

---
+++ Source/ProjectR/Public/UI/Menu.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Menu.generated.h"
+
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+  FVector2D buttonAbsoluteCenterPosition(UButton* aButton);
+
+
+public:
+      virtual bool Initialize() override;
+};
