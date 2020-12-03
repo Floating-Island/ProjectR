188038380d206fc14a4d2463d22c2d422a8887a5
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-03 13:08:01 -0300

    Created the project's player controller.

---
+++ Source/ProjectR/Public/PlayerController/ProjectRPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ProjectRPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API AProjectRPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
