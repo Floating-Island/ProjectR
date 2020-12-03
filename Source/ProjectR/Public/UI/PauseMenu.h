b43824f17618c040af78f58642cfee7f82273d2e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-03 11:56:06 -0300

    Created the pause menu class.

---
+++ Source/ProjectR/Public/UI/PauseMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Menu.h"
+#include "PauseMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API UPauseMenu : public UMenu
+{
+	GENERATED_BODY()
+	
+};
