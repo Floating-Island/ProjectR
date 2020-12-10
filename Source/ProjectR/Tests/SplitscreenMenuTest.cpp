2f54477dbd16a5424f9692e8003ae13101326f0e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-10 13:23:48 -0300

    Created a test to check that the splitscreen menu isn't null when instantiated.

--- Source/ProjectR/Tests/SplitscreenMenuTest.cpp
+++ Source/ProjectR/Tests/SplitscreenMenuTest.cpp
@@ -1,12 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#if WITH_DEV_AUTOMATION_TESTS
 
+#if WITH_DEV_AUTOMATION_TESTS
 
 #include "SplitscreenMenuTest.h"
 #include "Commands/SplitscreenMenuTestCommands.h"
+#include "UI/SplitscreenMenu.h"
 
+
+
+bool FUSplitscreenMenuIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	USplitscreenMenu* testMenu = NewObject<USplitscreenMenu>();
+	TestNotNull(TEXT("The splitscreen menu shouldn't be null after instantiating it."), testMenu);
+
+	return true;
+}
 
 
 
--- Source/ProjectR/Tests/SplitscreenMenuTest.h
+++ Source/ProjectR/Tests/SplitscreenMenuTest.h
@@ -16,6 +16,9 @@
 
 #if WITH_DEV_AUTOMATION_TESTS
 
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FUSplitscreenMenuIsntNullWhenInstantiatedTest, 
+	"ProjectR.SplitscreenMenu Tests.Unit.000: Isn't null when instantiated",
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 
 
