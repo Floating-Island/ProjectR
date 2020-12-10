f6e30fa6463b2467b1e6f6bcb7ba3addf6853410
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-10 13:20:58 -0300

    Created the splitscreen menu test files.

---
+++ Source/ProjectR/Tests/SplitscreenMenuTest.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "SplitscreenMenuTest.h"
+#include "Commands/SplitscreenMenuTestCommands.h"
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
