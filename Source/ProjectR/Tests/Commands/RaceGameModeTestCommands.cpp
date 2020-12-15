9eb12001b58bcfa5095e7573dee1d95726132103
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-15 16:13:30 -0300

    Fixed wrong parameter in log.

--- Source/ProjectR/Tests/Commands/RaceGameModeTestCommands.cpp
+++ Source/ProjectR/Tests/Commands/RaceGameModeTestCommands.cpp
@@ -389,9 +389,9 @@ bool FCheckJetsSameRotationAsTrack::Update()
 		{
 			float distanceBetweenJetAndTrack = testTrack->distanceAlongSplineOf(jet);
 			FRotator trackSectionRotation = testTrack->rotationAt(distanceBetweenJetAndTrack);
-			UE_LOG(LogTemp, Log, TEXT("track section rotation: %f."), trackSectionRotation);
+			UE_LOG(LogTemp, Log, TEXT("track section rotation: %s."), trackSectionRotation.ToString());
 			FRotator jetRotation = jet->GetActorRotation();
-			UE_LOG(LogTemp, Log, TEXT("jet rotation: %f."), jetRotation);
+			UE_LOG(LogTemp, Log, TEXT("jet rotation: %s."), jetRotation.ToString());
 			if (!jetRotation.Equals(trackSectionRotation))
 			{
 				jetsWithSameRotationAsTrackSections = false;
