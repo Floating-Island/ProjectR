b50214b478f40b2822154f80be194f238a03a932
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-22 13:32:41 -0300

    Improved Start network command implementation.

--- Source/ProjectR/Tests/Commands/NetworkCommands.cpp
+++ Source/ProjectR/Tests/Commands/NetworkCommands.cpp
@@ -26,17 +26,12 @@ bool FStartNetworkedPIECommand::Update()
 {
 	FLevelEditorModule& LevelEditorModule = FModuleManager::Get().GetModuleChecked<FLevelEditorModule>(TEXT("LevelEditor"));
 	
-	FRequestPlaySessionParams sessionParameters;
-
-	sessionParameters.DestinationSlateViewport = LevelEditorModule.GetFirstActiveViewport();//sets the server viewport in there. Otherwise, a window is created for the server.
-	
 	ULevelEditorPlaySettings* playSettings =  GetMutableDefault<ULevelEditorPlaySettings>();
-
 	playSettings->SetPlayNumberOfClients(numParticipants);
 	playSettings->SetPlayNetMode(netMode);
-	playSettings->PostEditChange();
-	playSettings->SaveConfig();
 
+	FRequestPlaySessionParams sessionParameters;
+	sessionParameters.DestinationSlateViewport = LevelEditorModule.GetFirstActiveViewport();//sets the server viewport in there. Otherwise, a window is created for the server.
 	sessionParameters.EditorPlaySettings = playSettings;	
 
 	GUnrealEd->RequestPlaySession(sessionParameters);
