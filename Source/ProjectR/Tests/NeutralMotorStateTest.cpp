3290307745e1c715321998a23f557b6ead015a56
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-31 15:04:48 -0300

    Created the test files for testing the motor states.

---
+++ Source/ProjectR/Tests/NeutralMotorStateTest.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "NeutralMotorStateTest.h"
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
---
+++ Source/ProjectR/Tests/ReversingMotorStateTest.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "ReversingMotorStateTest.h"
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
