9c9d72925b6641400a30c3483e7228961313202f
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-31 13:10:57 -0300

    Created the motorState class.

---
+++ Source/ProjectR/Private/Jet/MotorStates/MotorState.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Jet/MotorStates/MotorState.h"
+
+
+// Sets default values
+AMotorState::AMotorState()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AMotorState::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
