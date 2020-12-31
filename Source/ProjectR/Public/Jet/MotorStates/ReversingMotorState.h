812498016866ba2a45bfba702cbba03cda28c64e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2020-12-31 14:54:03 -0300

    Created the Accelerating, reversing and neutral motor states.

---
+++ Source/ProjectR/Public/Jet/MotorStates/ReversingMotorState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Jet/MotorStates/MotorState.h"
+#include "ReversingMotorState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API AReversingMotorState : public AMotorState
+{
+	GENERATED_BODY()
+	
+};
