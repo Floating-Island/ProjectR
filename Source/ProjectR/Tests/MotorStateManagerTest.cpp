7927c680322c3942b3f472186ad9181eb1e3b564
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-18 13:29:20 -0300

    Created files needed for the motor state manager tests.

---
+++ Source/ProjectR/Tests/MotorStateManagerTest.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "MotorStateManagerTest.h"
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
