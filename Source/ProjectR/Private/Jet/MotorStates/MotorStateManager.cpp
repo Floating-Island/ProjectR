63a8e85a7644c5b4ce849392187d552a7fed87a4
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-18 16:44:25 -0300

    Made the motor state manager replicate.

--- Source/ProjectR/Private/Jet/MotorStates/MotorStateManager.cpp
+++ Source/ProjectR/Private/Jet/MotorStates/MotorStateManager.cpp
@@ -13,6 +13,7 @@ AMotorStateManager::AMotorStateManager()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	SetReplicates(true);
 	motorState = nullptr;
 }
 
