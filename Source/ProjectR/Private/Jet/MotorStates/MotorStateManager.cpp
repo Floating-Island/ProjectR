f3bca9b8e42970b7c2402a9e7f18d012f7e028f1
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-18 21:06:26 -0300

    Made the motor state manager always relevant to the network.

--- Source/ProjectR/Private/Jet/MotorStates/MotorStateManager.cpp
+++ Source/ProjectR/Private/Jet/MotorStates/MotorStateManager.cpp
@@ -15,6 +15,7 @@ AMotorStateManager::AMotorStateManager()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	SetReplicates(true);
+	bAlwaysRelevant = true;
 	motorState = nullptr;
 }
 
