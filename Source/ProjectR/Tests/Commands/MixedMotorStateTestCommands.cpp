57164aede470afc57e3ee21f4a1a5d4777bea64c
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-25 16:55:35 -0300

    Command was expecting a floor mesh actor when none was necessary.

--- Source/ProjectR/Tests/Commands/MixedMotorStateTestCommands.cpp
+++ Source/ProjectR/Tests/Commands/MixedMotorStateTestCommands.cpp
@@ -52,17 +52,16 @@ bool FCheckMixedStateActivation::Update()
 		PIESessionUtilities sessionUtilities = PIESessionUtilities();
 		UWorld* testWorld = sessionUtilities.defaultPIEWorld();
 		AJet* testJet = sessionUtilities.retrieveFromPIEAnInstanceOf<AJet>();
-		AFloorMeshActor* testFloor = sessionUtilities.retrieveFromPIEAnInstanceOf<AFloorMeshActor>();
 
-		if (testJet && testFloor)
+		if (testJet)
 		{
 			float currentSpeed = testJet->currentSpeed();
 			bool sumIsBiggerThanZero = testJet->acceleration() - testJet->brakeValue() > 0;
 			bool speedHasBeenUpdated = (sumIsBiggerThanZero ? currentSpeed > previousSpeed : currentSpeed < previousSpeed) && !FMath::IsNearlyEqual(currentSpeed, previousSpeed, 0.1f);
 			speedHasBeenUpdated = (FMath::IsNearlyEqual(testJet->acceleration(), testJet->brakeValue())) ? true : speedHasBeenUpdated;// if they're the same value, assume that the speed has been updated.
 
-			UE_LOG(LogTemp, Log, TEXT("previous distance between floor and jet: %f"), previousSpeed);
-			UE_LOG(LogTemp, Log, TEXT("current distance between floor and jet: %f"), currentSpeed);
+			UE_LOG(LogTemp, Log, TEXT("previous speed: %f"), previousSpeed);
+			UE_LOG(LogTemp, Log, TEXT("current speed: %f"), currentSpeed);
 			UE_LOG(LogTemp, Log, TEXT("Jet acceleration value: %f"), testJet->acceleration());
 			UE_LOG(LogTemp, Log, TEXT("Jet brake value: %f"), testJet->brakeValue());
 			UE_LOG(LogTemp, Log, TEXT("acceleration and brake sum %s bigger than zero."), *FString(sumIsBiggerThanZero ? "is" : "isn't"));
