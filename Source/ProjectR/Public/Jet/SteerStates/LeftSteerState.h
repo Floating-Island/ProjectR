7ee5ac9133c3cebcbbbad155b9209985dbed3bc6
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-26 15:15:06 -0300

    Created the left, center and right steer states.

---
+++ Source/ProjectR/Public/Jet/SteerStates/LeftSteerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Jet/SteerStates/SteerState.h"
+#include "LeftSteerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ULeftSteerState : public USteerState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectR/Public/Jet/SteerStates/RightSteerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Jet/SteerStates/SteerState.h"
+#include "RightSteerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API URightSteerState : public USteerState
+{
+	GENERATED_BODY()
+	
+};
