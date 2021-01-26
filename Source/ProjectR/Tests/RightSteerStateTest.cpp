df968c954d9b131f3be23e4a0ea47cbe106ed1fe
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-26 15:25:40 -0300

    Created the necessary test files.

---
+++ Source/ProjectR/Tests/RightSteerStateTest.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "RightSteerStateTest.h"
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
