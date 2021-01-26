d77a037486d7d10271906d7aa67b1e4b4dae1e6a
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-26 01:53:18 -0300

    Created a test to check that releasing the brake key replicates the neutralize motor state.

--- Source/ProjectR/Tests/JetTest.cpp
+++ Source/ProjectR/Tests/JetTest.cpp
@@ -743,6 +743,29 @@ bool FAJetServerNeutralizesWhenReleasingAccelerationKeyTest::RunTest(const FStri
 }
 
 
+bool FAJetServerNeutralizesWhenReleasingBrakeKeyTest::RunTest(const FString& Parameters)
+{
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld-JetMOCKTest");
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+	int32 numberOfPlayers = 2;
+	EPlayNetMode networkMode = EPlayNetMode::PIE_ListenServer;
+
+	ADD_LATENT_AUTOMATION_COMMAND(FStartNetworkedPIESession(numberOfPlayers, networkMode));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FServerSpawnJet(numberOfPlayers));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FClientReleaseActionKey(FName(TEXT("BrakeAction")), numberOfPlayers));
+
+	int tickCount = 0;
+	int tickLimit = 10;
+	ADD_LATENT_AUTOMATION_COMMAND(FServerCheckJetNeutralMotorState(tickCount, tickLimit, numberOfPlayers, this));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
+	return true;
+}
+
+
 
 
 
--- Source/ProjectR/Tests/JetTest.h
+++ Source/ProjectR/Tests/JetTest.h
@@ -279,6 +279,12 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FAJetServerNeutralizesWhenReleasingAcceleration
 	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 
+//uses a mock
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FAJetServerNeutralizesWhenReleasingBrakeKeyTest, 
+	"ProjectR.Jet Tests.Replication.049: server neutralizes motor state when client releases brake key", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
 //jet mesh should be set to collision enabled. (query and physics or physics only).
 //delete tests that check velocity alignment to forward or backwards vectors after steering. Or see if it's possible to remade them.
 //create a state with state factory to handle the direction that the jet is heading (forwards or backwards). It should update itself every time that acceleration or brake is called.
