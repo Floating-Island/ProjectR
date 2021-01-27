e313e10a9ead7cde040ae7e21645a986b035418d
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-27 09:11:29 -0300

    Created the steer state manager mock.

---
+++ Source/ProjectR/Tests/Mocks/SteerStateManagerMOCK.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Jet/SteerStates/SteerStateManager.h"
+#include "SteerStateManagerMOCK.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ASteerStateManagerMOCK : public ASteerStateManager
+{
+	GENERATED_BODY()
+	
+};
