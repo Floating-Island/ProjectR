1f1a994b758d9ea37a0ae32c68a8de715b006a06
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-01-28 15:47:57 -0300

    Made the steer state unsupported for network replication. The following commit will reflect the same prior modifications made to the motor state and motor state manager.

--- Source/ProjectR/Public/Jet/SteerStates/SteerState.h
+++ Source/ProjectR/Public/Jet/SteerStates/SteerState.h
@@ -15,6 +15,5 @@ class PROJECTR_API USteerState : public UObject
 	GENERATED_BODY()
 
 public:
-	virtual bool IsSupportedForNetworking() const override;
 	virtual void activate(USteeringComponent* aSteeringComponent) PURE_VIRTUAL(AMotorState::activate, ; );//I don't like this. It should be =0 instead of a macro...
 };
