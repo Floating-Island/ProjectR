2c34a87090d5d511e820b4738f1ff6d9aedb7f5d
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-01 16:10:01 -0300

    Created a test to check that the server changes its steer state to Center when the client releases the right steer key.

--- Source/ProjectR/Tests/JetTest.h
+++ Source/ProjectR/Tests/JetTest.h
@@ -379,6 +379,12 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FAJetServerChangesSteerToCenterSteerWhenReleasi
 	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
 
 
+//uses a mock
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FAJetServerChangesSteerToCenterSteerWhenReleasingSteerRightKeyTest, 
+	"ProjectR.Jet Tests.Replication.066: server changes steer state to CenterSteer when client releases SteerRight key", 
+	EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter)
+
+
 //jet mesh should be set to collision enabled. (query and physics or physics only).
 //delete tests that check velocity alignment to forward or backwards vectors after steering. Or see if it's possible to remade them.
 //create a state with state factory to handle the direction that the jet is heading (forwards or backwards). It should update itself every time that acceleration or brake is called.
