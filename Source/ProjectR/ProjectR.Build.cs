e067f0cb074296563224f729af15400d73f15bd6
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-08 13:56:22 -0300

    Added required module.

--- Source/ProjectR/ProjectR.Build.cs
+++ Source/ProjectR/ProjectR.Build.cs
@@ -8,7 +8,7 @@ public class ProjectR : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UnrealEd", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UnrealEd", "UMG", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
