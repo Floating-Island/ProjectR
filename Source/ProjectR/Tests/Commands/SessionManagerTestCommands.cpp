7ad33a6b9efe8ba620130804f8c0f14a51c2c557
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-08 18:52:59 -0300

    Removed messages.

--- Source/ProjectR/Tests/Commands/SessionManagerTestCommands.cpp
+++ Source/ProjectR/Tests/Commands/SessionManagerTestCommands.cpp
@@ -27,14 +27,11 @@ bool FUSessionManagerCreateAndCheckSessionCreation::Update()
 			UWorld* containerWorld = testContainer->GetWorld();
 			if(ensure(containerWorld))
 			{
-				UE_LOG(LogTemp, Log, TEXT("trying to store session manager object"));
 				testContainer->storeObjectOfType<USessionManager>();
-				UE_LOG(LogTemp, Log, TEXT("session manager object stored"));
 				USessionManager* testManager = Cast<USessionManager, UObject>(testContainer->retrieveStoredObject());
 				if(testManager)
 				{
 					testManager->prepareSubsystemAndInterface();
-					UE_LOG(LogTemp, Log, TEXT("session manager object retrieved"));
 					test->TestTrue("createLANSession should start the asynchronous creation of a LAN session", testManager->createLANSession());
 
 					return true;
