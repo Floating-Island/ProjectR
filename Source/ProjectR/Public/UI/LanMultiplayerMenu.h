d8e45890f0cf805121f8dda4e85b75fc82d943c1
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-13 16:27:25 -0300

    Created the LAN multiplayer menu to handle LAN sessions.

---
+++ Source/ProjectR/Public/UI/LanMultiplayerMenu.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Menu.h"
+#include "LanMultiplayerMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ULanMultiplayerMenu : public UMenu
+{
+	GENERATED_BODY()
+	
+};
