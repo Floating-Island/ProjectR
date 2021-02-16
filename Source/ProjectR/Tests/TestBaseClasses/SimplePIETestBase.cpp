adbfa419e715de790869b4a52c379715eda3b226
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-16 13:15:54 -0300

    Created test class to minimize tests code.

---
+++ Source/ProjectR/Tests/TestBaseClasses/SimplePIETestBase.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SimplePIETestBase.h"
+#include "../Utilities/PIESessionUtilities.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+
+void FSimplePIETestBase::appendTestFailureWhen(bool aTickCountExceedsLimit)
+{
+	if(aTickCountExceedsLimit)
+	{
+		FString limitReachedMessage = FString(TEXT("Tick limit of %d reached. "), tickLimit) + testMessage;
+		TestFalse(*limitReachedMessage, aTickCountExceedsLimit);
+	}
+}
+
+void FSimplePIETestBase::pauseCurrentPIEWorldWhen(bool aTickCountExceedsLimit)
+{
+	if(aTickCountExceedsLimit)
+	{
+		PIESessionUtilities sessionUtilities = PIESessionUtilities();
+		sessionUtilities.currentPIEWorld()->bDebugFrameStepExecution = true;
+	}
+}
+
+void FSimplePIETestBase::increaseTickCount()
+{
+	++tickCount;
+}
+
+void FSimplePIETestBase::establishTestMessageTo(FString aMessage)
+{
+	if(testMessage.IsEmpty())
+	{
+		testMessage = aMessage;
+	}
+}
+
+void FSimplePIETestBase::establishTickLimitTo(int aDesiredQuantity)
+{
+	if(tickLimit == 0)
+	{
+		tickLimit = aDesiredQuantity;
+	}
+}
+
+bool FSimplePIETestBase::manageTickCountTowardsLimit()
+{
+	bool tickCountExceedsLimit = tickCount > tickLimit;
+
+	appendTestFailureWhen(tickCountExceedsLimit);
+	pauseCurrentPIEWorldWhen(tickCountExceedsLimit);
+	increaseTickCount();
+
+	return tickCountExceedsLimit;
+}
+
+FString FSimplePIETestBase::conditionMessage()
+{
+	return testMessage;
+}
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
---
+++ Source/ProjectR/Tests/TestBaseClasses/SimplePIETestBase.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Misc/AutomationTest.h"
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+
+/**
+ * Simple test class able to set a test message (useful for TestTrue, TestFalse, etc. statements).
+ * is able to set the test to failure if it reaches an established tick limit.
+ */
+class FSimplePIETestBase : public FAutomationTestBase
+{
+FString testMessage;
+int tickCount;
+int tickLimit;
+
+ void appendTestFailureWhen(bool aTickCountExceedsLimit);
+ void pauseCurrentPIEWorldWhen(bool aTickCountExceedsLimit);
+ void increaseTickCount();
+
+public:
+	FSimplePIETestBase(const FString& InName, const bool bInComplexTask)
+		: FAutomationTestBase(InName, bInComplexTask)
+	{
+        testMessage = FString();
+        tickCount = 0;
+        tickLimit = 0;
+	}
+
+/**
+ * method that only sets the test message if it's empty.
+ */
+void establishTestMessageTo(FString aMessage);
+
+/**
+ * method that only sets the tick limit on the first time it's called.
+ */
+void establishTickLimitTo(int aDesiredQuantity);
+
+/**
+ * checks that if the tick count reaches the tick limit.
+ * If true, appends a failure message to the test.
+ * returns whether the tick count exceeds the tick limit.
+ * Should be located at the bottom of the latent command where you want to start ticking to end.
+ * inside the latent command, it should be called like: return test->manageTickCountToLimit();...
+ */
+bool manageTickCountTowardsLimit();
+
+/**
+ * returns the test message.
+ */
+ FString conditionMessage();
+
+};
+
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
