d26c764f4fd1a596ae2ad04ba14cdd3f56345d94
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-17 15:08:15 -0300

    Made the string button scroll box populate its scroll box with string holder buttons.

--- Source/ProjectR/Private/UI/StringButtonScrollBox.cpp
+++ Source/ProjectR/Private/UI/StringButtonScrollBox.cpp
@@ -3,3 +3,26 @@
 
 #include "UI/StringButtonScrollBox.h"
 
+#include "UI/StringHolderButton.h"
+
+
+void UStringButtonScrollBox::populateBox(TArray<FString> aBunchOfStrings)
+{
+	if(scrollBoxWidget)
+	{
+		for(auto aString : aBunchOfStrings)
+		{
+			UStringHolderButton* stringButtonWidget = Cast<UStringHolderButton, UUserWidget>(CreateWidget(this, stringHolderButtonClass));
+			if(stringButtonWidget)
+			{
+				stringButtonWidget->store(aString);
+				scrollBoxWidget->AddChild(stringButtonWidget);
+			}
+		}
+	}
+}
+
+int UStringButtonScrollBox::stringButtonsQuantity()
+{
+	return scrollBoxWidget->GetChildrenCount();
+}
--- Source/ProjectR/Public/UI/StringButtonScrollBox.h
+++ Source/ProjectR/Public/UI/StringButtonScrollBox.h
@@ -6,6 +6,7 @@
 #include "Components/ScrollBox.h"
 #include "StringButtonScrollBox.generated.h"
 
+class UStringHolderButton;
 /**
  * 
  */
@@ -13,5 +14,15 @@ UCLASS()
 class PROJECTR_API UStringButtonScrollBox : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+		UScrollBox* scrollBoxWidget;
+
+	UPROPERTY(EditDefaultsOnly, Category = "String Holder Button Class")
+		TSubclassOf<UStringHolderButton> stringHolderButtonClass;
+
+public:
+	void populateBox(TArray<FString> aBunchOfStrings);
+	int stringButtonsQuantity();
 };
