d7d66bf0cf3c040a7c25b0fa2d44b81e3248304e
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-19 16:40:41 -0300

    Retrying tests with lan multiplayer menu to reassure that it's the root of the problem. If this succeeds, then the use of a game session name different than GameSessionName was the problem.

--- Source/ProjectR/Tests/LanMultiplayerMenuTest.cpp
+++ Source/ProjectR/Tests/LanMultiplayerMenuTest.cpp
@@ -9,61 +9,61 @@
 
 #include "UI/LanMultiplayerMenu.h"
 
-//bool FULanMultiplayerMenuIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
-//{
-//	ULanMultiplayerMenu* testLanMultiplayerMenu = NewObject<ULanMultiplayerMenu>();
-//	TestNotNull(TEXT("The LAN multiplayer menu shouldn't be null after instantiating it."), testLanMultiplayerMenu);
-//
-//	return true;
-//}
-//
-//
-//bool FULanMultiplayerMenuClickingGoBackButtonRemovesFromViewportTest::RunTest(const FString& Parameters)
-//{
-//	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
-//	establishTestMessageTo(FString("The lan multiplayer menu should be removed from viewport when clicking the go back button."));
-//	establishTickLimitTo(3);
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
-//	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FCheckLanMultiplayerMenuClickGoBackRemovesFromViewport(nullptr, false, this));
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
-//	return true;
-//}
-//
-//
-//bool FULanMultiplayerMenuClickingGoBackButtonBringsMainMenuTest::RunTest(const FString& Parameters)
-//{
-//	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
-//	establishTestMessageTo(FString("The lan multiplayer menu should be bring the main menu to viewport when clicking the go back button."));
-//	establishTickLimitTo(3);
-//	
-//	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
-//	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FCheckLanMultiplayerMenuClickGoBackBringsMainMenu(nullptr, this));
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
-//	return true;
-//}
-//
-//
-//bool FULanMultiplayerMenuClickingCreateSessionButtonStartsSessionTest::RunTest(const FString& Parameters)
-//{
-//	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
-//	establishTestMessageTo(FString("When clicking the create session button the lan multiplayer menu should travel to lobby (after session starts)."));
-//	establishTickLimitTo(10);
-//	
-//	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
-//	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FCheckLanMultiplayerMenuClickCreateSessionBringsLobby(nullptr, false, this));
-//
-//	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
-//	return true;
-//}
+bool FULanMultiplayerMenuIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	ULanMultiplayerMenu* testLanMultiplayerMenu = NewObject<ULanMultiplayerMenu>();
+	TestNotNull(TEXT("The LAN multiplayer menu shouldn't be null after instantiating it."), testLanMultiplayerMenu);
+
+	return true;
+}
+
+
+bool FULanMultiplayerMenuClickingGoBackButtonRemovesFromViewportTest::RunTest(const FString& Parameters)
+{
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
+	establishTestMessageTo(FString("The lan multiplayer menu should be removed from viewport when clicking the go back button."));
+	establishTickLimitTo(3);
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FCheckLanMultiplayerMenuClickGoBackRemovesFromViewport(nullptr, false, this));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
+	return true;
+}
+
+
+bool FULanMultiplayerMenuClickingGoBackButtonBringsMainMenuTest::RunTest(const FString& Parameters)
+{
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
+	establishTestMessageTo(FString("The lan multiplayer menu should be bring the main menu to viewport when clicking the go back button."));
+	establishTickLimitTo(3);
+	
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FCheckLanMultiplayerMenuClickGoBackBringsMainMenu(nullptr, this));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
+	return true;
+}
+
+
+bool FULanMultiplayerMenuClickingCreateSessionButtonStartsSessionTest::RunTest(const FString& Parameters)
+{
+	FString testWorldName = FString("/Game/Tests/TestMaps/VoidWorld");
+	establishTestMessageTo(FString("When clicking the create session button the lan multiplayer menu should travel to lobby (after session starts)."));
+	establishTickLimitTo(10);
+	
+	ADD_LATENT_AUTOMATION_COMMAND(FEditorLoadMap(testWorldName));
+	ADD_LATENT_AUTOMATION_COMMAND(FStartPIECommand(true));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FCheckLanMultiplayerMenuClickCreateSessionBringsLobby(nullptr, false, this));
+
+	ADD_LATENT_AUTOMATION_COMMAND(FEndPlayMapCommand);
+	return true;
+}
 
 
 
