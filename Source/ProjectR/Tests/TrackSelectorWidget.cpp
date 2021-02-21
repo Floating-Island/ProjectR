29d5352cff9eea85d4b4924309ce63148a9f81f1
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-21 17:22:01 -0300

    Created the track selector and checked that it isn't null when instantiated.

---
+++ Source/ProjectR/Tests/TrackSelectorWidget.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+#include "TrackSelectorWidget.h"
+#include "../Public/UI/TrackSelectorWidget.h"
+
+
+
+bool FUTrackSelectorWidgetIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	UTrackSelectorWidget* testSelector = NewObject<UTrackSelectorWidget>();
+
+	TestNotNull(TEXT("Isn't null when instantiated."), testSelector);
+	
+	return true;
+}
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
