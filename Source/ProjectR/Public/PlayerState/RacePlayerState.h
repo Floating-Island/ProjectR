bae9dd7e178f2df3ec27cb54eb43b78ba68257e8
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-02-24 14:45:51 -0300

    Created the RacePlayerState to be able to have the current position and lap of the jet while in the game.

---
+++ Source/ProjectR/Public/PlayerState/RacePlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "RacePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ARacePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectR/Tests/RacePlayerStateTest.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "RacePlayerStateTest.h"
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
