0b4cc5f10d233f9958ee054016cc9808d9cc9e73
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-03-04 20:54:46 -0300

    Created the lobby game state to help transmit info from server to clients.

---
+++ Source/ProjectR/Public/GameState/LobbyGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "LobbyGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTR_API ALobbyGameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectR/Tests/LobbyGameStateTest.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../Public/GameState/LobbyGameState.h"
+#if WITH_DEV_AUTOMATION_TESTS
+
+
+#include "LobbyGameStateTest.h"
+
+
+bool FALobbyGameStateIsntNullWhenInstantiatedTest::RunTest(const FString& Parameters)
+{
+	ALobbyGameState* testState = NewObject<ALobbyGameState>();
+
+	TestNotNull(FString("Isn't null when instantiated"), testState);
+
+	return true;
+}
+
+
+
+
+
+#endif //WITH_DEV_AUTOMATION_TESTS
