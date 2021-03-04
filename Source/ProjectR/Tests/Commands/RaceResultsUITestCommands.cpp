7bfb9e8fd72dae4292a6b9478d6ec79dd8aa0108
Alberto Nicolás Mikulan <betomiku@gmail.com>
2021-03-04 11:03:04 -0300

    Modified command to add the race results to viewport and added necessary blueprints for test.

--- Source/ProjectR/Tests/Commands/RaceResultsUITestCommands.cpp
+++ Source/ProjectR/Tests/Commands/RaceResultsUITestCommands.cpp
@@ -26,9 +26,19 @@ bool FCheckRaceResultsInfoBoxQuantitySameAsPlayerStates::Update()
 		if(testContainer)
 		{
 			URaceResultsUI* testResults = Cast<URaceResultsUI, UUserWidget>(testContainer->retrieveWidget());
-			AProjectRGameState* testGameState = sessionUtilities.retrieveFromPIEAnInstanceOf<AProjectRGameState>();
+			if(!testResults->IsInViewport())
+			{
+				testResults->AddToViewport();
+			}
 
-			bool QuantitiesCoincident = testResults->rowsQuantity() == testGameState->PlayerArray.Num();
+			int resultsQuantity = testResults->rowsQuantity();
+			UE_LOG(LogTemp, Log, TEXT("quantity of results shown: %d"), resultsQuantity);
+
+			AProjectRGameState* testGameState = sessionUtilities.retrieveFromPIEAnInstanceOf<AProjectRGameState>();
+			int gameStatePlayers = testGameState->PlayerArray.Num();
+			UE_LOG(LogTemp, Log, TEXT("quantity of players in gameState: %d"), gameStatePlayers);
+			
+			bool QuantitiesCoincident = resultsQuantity == gameStatePlayers;
 			if(QuantitiesCoincident)
 			{
 				test->TestTrue(test->conditionMessage(), QuantitiesCoincident);
